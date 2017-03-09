879c64549ad2e96bd8bdc75fd74bae2e75b26246
folkvir <dev.arnaudgrall@gmail.com>
2017-03-09 16:39:30 +0100

    up to 0.4.6

--- tests/laddaTest.js
+++ tests/laddaTest.js
@@ -1,6 +1,5 @@
 'use strict';
 require('chai').should();
-const Spray = require('spray-wrtc');
 const NDP = require('../foglet-ndp.js').NDP;
 // const $ = require('jquery');
 const endpoint = 'https://query.wikidata.org/bigdata/ldf';
@@ -21,32 +20,14 @@ const requests = [
 describe('[LADDA]', function () {
 	this.timeout(15000);
 	it('Initialization', function (done) {
-		// $.ajax({
-		// 	url : 'https://service.xirsys.com/ice',
-		// 	data : {
-		// 		ident: 'folkvir',
-		// 		secret: 'a0fe3e18-c9da-11e6-8f98-9ac41bd47f24',
-		// 		domain: 'foglet-examples.herokuapp.com',
-		// 		application: 'foglet-examples',
-		// 		room: 'test',
-		// 		secure: 1
-		// 	}
-		// }).then(function (response, status) {
-		// 	console.log(status);
-		// 	console.log(response);
-		//
-		// 	var iceServers = [];
-		// 	if (response.d.iceServers) {
-		// 		iceServers = response.d.iceServers;
-		// 	}
-
 		const f1 = new NDP({
 			protocol: 'testNdp',
 			webrtc:	{
 				trickle: false,
 				iceServers: []
 			},
-			room: 'test'
+			room: 'test',
+			verbose:true
 		});
 		const f2 = new NDP({
 			protocol: 'testNdp',
@@ -54,7 +35,8 @@ describe('[LADDA]', function () {
 				trickle: false,
 				iceServers: []
 			},
-			room: 'test'
+			room: 'test',
+			verbose:true
 		});
 
 		const f3 = new NDP({
@@ -63,20 +45,18 @@ describe('[LADDA]', function () {
 				trickle: false,
 				iceServers: []
 			},
-			room: 'test'
+			room: 'test',
+			verbose:true
 		});
 
 
 		let cpt = 0;
 		const nbResultWantetd = 11;
 		f1.events.on('ndp-answer', (response) => {
-			console.log(response)
+			console.log(response);
 			cpt++;
 			console.log('Number of answer : ' + cpt);
-			// assert response fields
-			//response.should.include.keys('type', 'id', 'schedulerId', 'payload', 'endpoint', 'query',
-				//'sendQueryTime', 'receiveQueryTime', 'startExecutionTime', 'endExecutionTime', 'sendResultsTime', 'receiveResultsTime');
-			// assert number of answers
+
 			if(cpt >= nbResultWantetd) {
 				done();
 			}
@@ -84,18 +64,16 @@ describe('[LADDA]', function () {
 
 		f1.init();
 		f2.init();
+		f3.init();
 
 		f1.connection().then(() =>  {
 			f2.connection().then( () => {
-				f3.init();
 				f3.connection().then( () => {
-					return f1.send(requests, endpoint);
+					f1.send(requests, endpoint);
 				}).catch(error => {
-					console.log(error)
 					done(error);
 				});
-			})
+			});
 		});
-		// });
 	});
 });
