2b6d63bf3a38543b1cac24914236177d1d076e70
folkvir <dev.arnaudgrall@gmail.com>
2017-04-11 10:49:49 +0200

    switch to ERROR lvl for ldf

--- build/foglet-ndp.bundle.js
+++ build/foglet-ndp.bundle.js
@@ -70661,7 +70661,7 @@ const moment = require('moment');
 const _ = require('lodash');
 
 // LDF LOG Disabling
-ldf.Logger.setLevel('EMERGENCY');
+ldf.Logger.setLevel('ERROR');
 // ldf.Logger.setLevel('DEBUG');
 // status
 const STATUS_DELEGATED = 'status_delegated';
